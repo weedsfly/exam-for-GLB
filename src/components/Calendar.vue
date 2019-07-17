4a133ef8b917423529356a2c43e9e78231b73723
laiyj <weedsfly@163.com>
2019-07-17 09:58:37 +0800

    init project

---
+++ src/components/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        日历
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Calendar from '@/components/Calendar'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Calendar',
+      component: Calendar
+    },
+    {
+      path: '/calendar',
+      name: 'Calendar',
+      component: Calendar
+    }
+  ]
+})
