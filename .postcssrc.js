af91a971f7699fb67e2b21ffaa2f5a22815e810e
weedsfly <weedsfly@163.com>
2019-07-17 15:41:49 +0800

    postcss config

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,14 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss-nested": {},
+    "postcss-px2rem-exclude": {
+      remUnit: 75,
+      exclude: /node_modules/i
+    },
+    "cssnano": {
+      zindex: false
+    }
   }
 }
