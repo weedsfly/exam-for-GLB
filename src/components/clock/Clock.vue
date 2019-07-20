a3d94bc1743cda7032a47dfe134d836178374072
weedsfly <weedsfly@163.com>
2019-07-20 17:42:06 +0800

    clock work

--- src/components/clock/Clock.vue
+++ src/components/clock/Clock.vue
@@ -30,6 +30,35 @@ export default {
       minuteRotate: 'rotatez(0deg)',
       secondRotate: 'rotatez(0deg)'
     }
+  },
+  methods: {
+    show () {
+      this.showTime()
+      if (this._timer) clearInterval(this._timer)
+      this._timer = setInterval(() => {
+        this.showTime()
+      }, 1000)
+    },
+    showTime () {
+      const now = new Date()
+      let times = [now.getHours(), now.getMinutes(), now.getSeconds()]
+      let hour = +times[0]
+      hour = hour > 11 ? hour - 12 : hour
+      let minute = +times[1]
+      let second = +times[2] || 0
+      let hourAngle = hour * 30 + minute * 6 / 360 * 30
+      let minuteAngle = minute * 6
+      let secondAngle = second * 6
+      this.hourRotate = `rotatez(${hourAngle}deg)`
+      this.minuteRotate = `rotatez(${minuteAngle}deg)`
+      this.secondRotate = `rotatez(${secondAngle}deg)`
+    }
+  },
+  mounted () {
+    this.show()
+  },
+  destroyed () {
+    if (this._timer) clearInterval(this._timer)
   }
 }
 </script>
